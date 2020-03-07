05e485dbed4b6080434eb53ece5e4272f29ede64
iamnathanj <jacobs@heysparkbox.com>
2020-03-07 09:51:07 -0500

    bump deno std version v0.35.0

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { walk, WalkOptions } from "https://deno.land/std@v0.34.0/fs/mod.ts";
-export { delay } from "https://deno.land/std@v0.34.0/util/async.ts";
+export { walk, WalkOptions } from "https://deno.land/std@v0.35.0/fs/mod.ts";
+export { delay } from "https://deno.land/std@v0.35.0/util/async.ts";
