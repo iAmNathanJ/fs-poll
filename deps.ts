2ff14c7561ecbcc94bc7de530d1c657b04ca30ff
iamnathanj <jacobs@heysparkbox.com>
2020-03-11 22:56:32 -0400

    deno v0.36.0

--- deps.ts
+++ deps.ts
@@ -1,2 +1,6 @@
-export { walk, WalkOptions } from "https://deno.land/std@v0.35.0/fs/mod.ts";
-export { delay } from "https://deno.land/std@v0.35.0/util/async.ts";
+export {
+  walk,
+  WalkOptions,
+  WalkInfo
+} from "https://deno.land/std@v0.36.0/fs/mod.ts";
+export { delay } from "https://deno.land/std@v0.36.0/util/async.ts";
--- makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: test
-.PHONY: fmt
-
-test:
-	deno test --allow-read --allow-write --allow-env
-
-fmt:
-	deno fmt *.ts
